443d6ab3bc58f7d52da1b84b5a7a8e9d30ae187b
Stackingrule <messageswitching@gmail.com>
2020-03-15 23:25:40 +0800

    IDEA  红色警告问题

--- content-center/src/main/java/com/stackingrule/contentcenter/TestController.java
+++ content-center/src/main/java/com/stackingrule/contentcenter/TestController.java
@@ -13,13 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 
 @Slf4j
 @RestController
 public class TestController {
-    @Autowired(required = false)
+
+    // @Autowired(required = false)
+    @Resource
     private ShareMapper shareMapper;
 
     @GetMapping("/test")
