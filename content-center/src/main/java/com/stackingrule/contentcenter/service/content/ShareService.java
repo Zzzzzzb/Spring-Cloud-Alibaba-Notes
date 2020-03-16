77ee1efc32c6384b54b9d916de4b6cbe6a482a37
Stackingrule <messageswitching@gmail.com>
2020-03-16 21:07:17 +0800

    为内容中心引入服务发现

--- content-center/src/main/java/com/stackingrule/contentcenter/service/content/ShareService.java
+++ content-center/src/main/java/com/stackingrule/contentcenter/service/content/ShareService.java
@@ -5,11 +5,17 @@ import com.stackingrule.contentcenter.domain.dto.content.ShareDTO;
 import com.stackingrule.contentcenter.domain.dto.user.UserDTO;
 import com.stackingrule.contentcenter.domain.entity.content.Share;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ShareService {
@@ -18,15 +24,25 @@ public class ShareService {
 
     private final RestTemplate restTemplate;
 
+    private DiscoveryClient discoveryClient;
+
     public ShareDTO findById(Integer id) {
         // 获取分享详情
         Share share = this.shareMapper.selectByPrimaryKey(id);
         // 发布人 id
         Integer userId = share.getUserId();
 
-        //
+        // 用户中心所有实例信息
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-center");
+        String targetURL = instances.stream()
+                .map(instance -> instance.getUri().toString() + "/users/{id}")
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前没有实例!"));
+
+        log.info("请求目标地址： {}", targetURL );
+
         UserDTO userDTO = restTemplate.getForObject(
-                "http://localhost:8080/users/{id}",
+                targetURL,
                 UserDTO.class,
                 userId
         );
