d621f410c9a07cc2ebab686701ce9dcf25ec2659
Stackingrule <messageswitching@gmail.com>
2020-03-17 19:56:09 +0800

    Java代码方式-指定日志级别

---
+++ content-center/src/main/java/com/stackingrule/contentcenter/configuration/UserFeignConfiguration.java
@@ -0,0 +1,18 @@
+package com.stackingrule.contentcenter.configuration;
+
+/**
+ * feign 配置类
+ * 这个类别加@Configuration注解
+ */
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+public class UserFeignConfiguration {
+
+    @Bean
+    public Logger.Level level() {
+        return Logger.Level.FULL;
+    }
+
+}
--- content-center/src/main/java/com/stackingrule/contentcenter/feignclient/UserCenterFeignClient.java
+++ content-center/src/main/java/com/stackingrule/contentcenter/feignclient/UserCenterFeignClient.java
@@ -1,11 +1,12 @@
 package com.stackingrule.contentcenter.feignclient;
 
+import com.stackingrule.contentcenter.configuration.UserFeignConfiguration;
 import com.stackingrule.contentcenter.domain.dto.user.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-center")
+@FeignClient(name = "user-center", configuration = UserFeignConfiguration.class)
 public interface UserCenterFeignClient {
 
     /**
