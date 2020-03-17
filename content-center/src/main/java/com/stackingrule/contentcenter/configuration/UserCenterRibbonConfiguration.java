3fd800bd1598390a4ecc7623c7d09d6f6429d4f7
Stackingrule <messageswitching@gmail.com>
2020-03-17 16:33:11 +0800

    细粒度配置自定义-全局方式

--- content-center/src/main/java/com/stackingrule/contentcenter/configuration/UserCenterRibbonConfiguration.java
+++ content-center/src/main/java/com/stackingrule/contentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,11 +1,11 @@
 package com.stackingrule.contentcenter.configuration;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
 
 @Configuration
-@RibbonClient(name = "user-center", configuration = RibbonConfiguration.class)
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 
 }
--- user-center/src/main/java/com/stackingrule/usercenter/controller/user/UserController.java
+++ user-center/src/main/java/com/stackingrule/usercenter/controller/user/UserController.java
@@ -3,22 +3,24 @@ package com.stackingrule.usercenter.controller.user;
 import com.stackingrule.usercenter.domain.entity.user.User;
 import com.stackingrule.usercenter.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
-@RequiredArgsConstructor(onConstructor = @_(@Autowired))
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{id}")
     public User findById(@PathVariable Integer id) {
-
+        log.info("我被请求!!!");
         return this.userService.findById(id);
     }
 }
