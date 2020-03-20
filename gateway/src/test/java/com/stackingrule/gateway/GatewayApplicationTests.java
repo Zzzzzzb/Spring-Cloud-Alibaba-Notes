7bd649dbd887a3c68f499966c842a2ee3252c1b3
Stackingrule <messageswitching@gmail.com>
2020-03-20 10:36:02 +0800

    gateway init

---
+++ gateway/src/test/java/com/stackingrule/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.stackingrule.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- user-center/src/main/java/com/stackingrule/usercenter/rocketmq/AddBonusStreamConsumer.java
+++ user-center/src/main/java/com/stackingrule/usercenter/rocketmq/AddBonusStreamConsumer.java
@@ -1,10 +1,6 @@
 package com.stackingrule.usercenter.rocketmq;
 
-import com.stackingrule.usercenter.dao.bonus.BonusEventLogMapper;
-import com.stackingrule.usercenter.dao.user.UserMapper;
 import com.stackingrule.usercenter.domain.dto.messaging.UserAddBonusMsgDTO;
-import com.stackingrule.usercenter.domain.entity.bonus.BonusEventLog;
-import com.stackingrule.usercenter.domain.entity.user.User;
 import com.stackingrule.usercenter.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,15 +9,11 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AddBonusStreamConsumer {
 
-    private final UserMapper userMapper;
-    private final BonusEventLogMapper bonusEventLogMapper;
     private final UserService userService;
 
     @StreamListener(Sink.INPUT)
